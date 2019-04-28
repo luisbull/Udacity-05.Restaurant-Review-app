1feab85a228133dfa23b74dbbcb52313e62f2906
Luis <luisbull@hotmail.com>
2019-04-28 11:11:11 +0100

    mapboxtoken added
    API  for mapbox added

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibHVpc2J1bGwiLCJhIjoiY2p2MDY4bWN0MDB1aDQ0cG9sbThhY2ZnMSJ9.UXNP8onVz0TOlTZAh1vBog',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
